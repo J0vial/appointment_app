1276da91c8755a206c42286732f15d598513f9be
Md Shakibul Alam <70951378+Alam2002@users.noreply.github.com>
2022-09-15 02:10:41 +0600

    update linking redirect

--- appointment/authentication/forms.py
+++ appointment/authentication/forms.py
@@ -9,15 +9,19 @@ class NewUserForm(UserCreationForm):
         (3,'Others')
     )
     Phone_no = forms.IntegerField(required=True)
+    first_name = forms.CharField(label = "First name")
+    last_name = forms.CharField(label = "Last name")
     email = forms.EmailField(required=True)
     gender = forms.ChoiceField(choices=gnd,required=True)
     class Meta:
         model = User
-        fields = ("username","email","Phone_no","gender","password1","password2")
+        fields = ("username","first_name","last_name","email","Phone_no","gender","password1","password2")
     
     def save(self,commit = True):
         user = super(NewUserForm, self).save(commit = False)
-        user.p_num = self.cleaned_data['Phone_no']
+        user.Phone_no = self.cleaned_data['Phone_no']
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
         user.email = self.cleaned_data['email']
         user.gender = self.cleaned_data['gender']
         if commit:
--- appointment/authentication/views.py
+++ appointment/authentication/views.py
@@ -1,7 +1,6 @@
 from contextlib import redirect_stderr
 from urllib import request
 from django.shortcuts import redirect, render
-import authentication
 from .forms import NewUserForm
 from django.contrib.auth import login,authenticate
 from django.contrib import messages
@@ -17,11 +16,12 @@ def register_request(request):
             user = form.save()
             login(request, user)
             messages.success(request, "Registration successfull.")
-            return redirect("authentication/login.html")
+            return redirect("authentication:login")
         
         messages.error(request,"Unsuccessfull login!")
+    else:
     
-    form = NewUserForm()        
+        form = NewUserForm()        
     return render(request=request, template_name="authentication/register.html", context={"register_form":form})
 
 
@@ -35,18 +35,20 @@ def login_request(request):
             username = form.cleaned_data.get('username')
             password = form.cleaned_data.get('passowrd')
             
+            
             user = authenticate(username = username, password = password)
             if user is not None:
                 login(request, user)
                 messages.info(request, f"You are logged in as {username}.")
-                return redirect ('main/home.html')
+                return redirect ('main:homepage')
             else:
                 messages.error(request, "Invalid username or password")
         else:
             
             messages.error(request, "You dont have an account")
+    else:
             
-    form = AuthenticationForm()
+        form = AuthenticationForm()
     return render(request, "authentication/login.html",context = {'login_form': form})
             
             
