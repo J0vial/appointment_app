807094593650399e6b85ea748a3a4b752f16bedb
Md Shakibul Alam <70951378+Alam2002@users.noreply.github.com>
2022-09-16 01:30:34 +0600

    remove unnecessary things in urls

--- appointment/authentication/urls.py
+++ appointment/authentication/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     
     path('register/',views.register_request , name  = "register"),
     path('login/',views.login_request , name = 'login'),
-    path('lgin/<int:pk>/',views.login_request , name = 'login')
+    
 ]
